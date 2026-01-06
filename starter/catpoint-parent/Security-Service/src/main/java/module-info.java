46bffe36e02a58664d265857ac468872bc6a5a7b
Bhuvana57 <130349892+Bhuvana57@users.noreply.github.com>
2026-01-06 14:33:01 +0000

    Fix Security-Service module-info: add all required modules

--- starter/catpoint-parent/Security-Service/src/main/java/module-info.java
+++ starter/catpoint-parent/Security-Service/src/main/java/module-info.java
@@ -4,5 +4,11 @@ module com.udacity.catpoint.security {
     requires java.prefs;
     requires com.google.common;
     requires com.google.gson;
+    requires java.sql;
+    
     opens com.udacity.catpoint.security.data to com.google.gson;
+    
+    exports com.udacity.catpoint.security.service;
+    exports com.udacity.catpoint.security.application;
+    exports com.udacity.catpoint.security.data;
 }
