593c28ac3233c02a5916af9d59df48f78eaf356d
Bhuvana57 <130349892+Bhuvana57@users.noreply.github.com>
2026-01-06 14:04:30 +0000

    Fix: Add java.desktop to Image-Service module-info

--- starter/catpoint-parent/Image-Service/src/main/java/module-info.java
+++ starter/catpoint-parent/Image-Service/src/main/java/module-info.java
@@ -1,10 +1,5 @@
 module com.udacity.catpoint.image {
-    exports com.udacity.image.service;
-    requires software.amazon.awssdk.auth;
-    requires software.amazon.awssdk.core;
-    requires software.amazon.awssdk.regions;
-    requires software.amazon.awssdk.rekognition;
-    requires software.amazon.awssdk.services.rekognition;
-    requires java.desktop;
+    exports com.udacity.catpoint.service;
     requires org.slf4j;
+    requires java.desktop;  // For BufferedImage (java.awt.image)
 }
